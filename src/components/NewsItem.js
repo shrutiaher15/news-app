29570b5307778b6649d13ea76ec2a6d503101897
ShrutiAher <shruti.aher@thoughtworks.com>
2022-06-14 19:50:16 +0530

    dynamically fetching news using fetch API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src={imageURL} className="card-img-top" alt="..." />
+                    <img src={!imageURL ? "https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png" : imageURL} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
-                        <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsURL} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
