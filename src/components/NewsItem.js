232002da5c33214aa1b8abd21387bcb10a9ff100
ShrutiAher <shruti.aher@thoughtworks.com>
2022-06-14 10:11:56 +0530

    Add. state in react class based component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
 
     render() {
-        let { title, description } = this.props;
+        let { title, description, imageURL, newsURL } = this.props;
         return (
             <div>
                 <div className="card" style={{ width: '18rem' }}>
-                    <img src="https://www.coindesk.com/resizer/xa-QJd3kqoIj8Z6xUY6l5SfoAvw=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/TCPTTJ54WREAJEUNX6WNW2JXTE.jpg" className="card-img-top" alt="..." />
+                    <img src={imageURL} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/news-detail" className="btn btn-primary">Go somewhere</a>
+                        <a href="/news-detail" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
             </div>
