0beefe87cbe39fe1a6ad688b0f9f50b9cb66e22c
shrutiaher15 <shrutiavinashaher@gmail.com>
2022-06-15 12:45:36 +0530

    Modifed source badge

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,7 +7,10 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card">
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill text-bg-info" style={{ left: '90%', zIndex: '1' }}>{source}</span>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                        <span className="badge rounded-pill text-bg-info">{source}</span>
+                    </div>
+
                     <img src={!imageURL ? "https://media4.s-nbcnews.com/i/newscms/2019_01/2705191/nbc-social-default_b6fa4fef0d31ca7e8bc7ff6d117ca9f4.png" : imageURL} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
@@ -16,7 +19,7 @@ export class NewsItem extends Component {
                         <a href={newsURL} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">Read More</a>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
